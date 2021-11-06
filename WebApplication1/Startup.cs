d579b3158ef6929d02bcbaa78fc69b9203461e13
IT20206178 <77095664+IT20206178@users.noreply.github.com>
2021-11-06 10:08:07 +0530

    web

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -20,15 +20,11 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDefaultFiles();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
+            app.UseStaticFiles(); 
+
         }
     }
 }
